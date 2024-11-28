ffff4229513deff718285371636097e8f38952d4
blueisfresh <elias.guerematchi.vrabl@gmail.com>
2024-11-28 13:55:09 +0100

    Database Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "../server/db"; // Adjust the path as needed
 
 const mockUrls = [
   "https://utfs.io/f/MOA66ou6ZmXlYsDU3b0yzPBVXvwFGD5n1ps2LbIcMxue60Yl",
@@ -12,10 +13,15 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+
         {[...mockImages, ...mockImages, ...mockImages].map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url} alt="image" />
