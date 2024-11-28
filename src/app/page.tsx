1ba6dbfd6c65dbaf1b768776267013943cd26271
blueisfresh <elias.guerematchi.vrabl@gmail.com>
2024-11-28 11:19:50 +0100

    scafold UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/MOA66ou6ZmXlYsDU3b0yzPBVXvwFGD5n1ps2LbIcMxue60Yl",
+  "https://utfs.io/f/MOA66ou6ZmXlfQ6umfa8MJPjWIdvzXmDGpUCu7s2tYScEZgR",
+  "https://utfs.io/f/MOA66ou6ZmXlz5InCEZOCHhFtaryTRxS5iGDk8sp2YQJeXw4",
+  "https://utfs.io/f/MOA66ou6ZmXlJaWakevzOlJ9FvDmuaey8k2QtUVRKE0ZoxXA",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello Gallery in Progress
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt="image" />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
